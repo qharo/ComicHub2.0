195ae0a04204a18def8ac8e761a68a2045dad8b9
qharo <aditya.nair24@gmail.com>
2020-03-30 20:14:37 +0530

    Third commit, with procfile. Hope this works

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log('this is working');
 const http = require('http');
+const express = require('express');
+app = express();
+
+app.get('/', (req, res, err) => {
+    res.set(200).json({
+        message: 'this is working'
+    })
+});
+
+module.exports = app;
